3bcd9553a72be44a5c0ce4629601589efac19e85
Vr3n <vickyspatel@gmail.com>
2023-08-20 17:00:04 +0530

    Added Fixtures and DB setup for tests

--- vcommerce/dashboard/tests/test_selenium_dashboard.py
+++ vcommerce/dashboard/tests/test_selenium_dashboard.py
@@ -1,15 +1,15 @@
 import pytest
+from django.contrib.auth.models import User
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
 @pytest.mark.selenium
-def test_create_new_admin_user(create_admin_user):
-    assert create_admin_user.__str__() == "admin"
+def test_dashboard_admin_login(live_server, django_db_fixture_setup, edge_browser_instance):
 
+    i = User.objects.get(id=1)
+    print(i.password)
 
-@pytest.mark.selenium
-def test_dashboard_admin_login(live_server, create_admin_user, edge_browser_instance):
     browser = edge_browser_instance
 
     browser.get(("%s%s" % (live_server.url, "/admin/login/")))
--- vcommerce/tests/fixtures.py
+++ vcommerce/tests/fixtures.py
@@ -1,12 +1,12 @@
 import pytest
+from django.core.management import call_command
 
 
-@pytest.fixture
-def create_admin_user(django_user_model):
+@pytest.fixture(scope="session")
+def django_db_fixture_setup(django_db_setup, django_db_blocker):
     """
-    Return new admin user.
+    Load DB data Fixtures.
     """
 
-    return django_user_model.objects.create_superuser(username="admin",
-                                                      email="a@a.com",
-                                                      password="password")
+    with django_db_blocker.unblock():
+        call_command("loaddata", "db_admin_fixture.json")
